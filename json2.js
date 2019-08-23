983db93b680bb52b8f2e3d0b9b2232d1d14764ad
John <test@gmail.com>
2019-08-23 23:33:23 +0100

    update

---
+++ json2.js
@@ -0,0 +1,16 @@
+/* parse json array */
+var requestURL = 'https://newschapmj1.github.io/testweb1/test3.json'
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  var walks = request.response;
+  console.log(walks);
+  //populateHeader(walks);
+  //showWalk(walks);
+  //var obj = JSON.parse(walks);
+  console.log(walks[0]);
+}
